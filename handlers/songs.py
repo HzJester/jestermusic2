b5935d704d6ced870d22ed71463d6f6c6cd272fc
HzJester <86559859+HzJester@users.noreply.github.com>
2021-12-25 23:13:00 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -1,4 +1,4 @@
-# Veez Music Bot (https://t.me/veezmusicbot)
+# jester Music Bot (https://t.me/jestermusicbot)
 
 import os
 import aiohttp
@@ -20,13 +20,13 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 
-@Client.on_message(filters.command("song") & ~filters.edited)
+@Client.on_message(filters.command("sarki") & ~filters.edited)
 async def song(client, message):
-    cap = "@OdaRobot"
+    cap = "@JesterMusicBot"
     url = message.text.split(None, 1)[1]
-    rkp = await message.reply("Processing...")
+    rkp = await message.reply("İşleniyor...")
     if not url:
-        await rkp.edit("**What song you Find?**\nUsage`/song <tittle>`")
+        await rkp.edit("**Hangi şarkıyı buldun?**\nKullanım`/sarki <başlık>`")
     search = SearchVideos(url, offset=1, mode="json", max_results=1)
     test = search.result()
     p = json.loads(test)
@@ -34,7 +34,7 @@ async def song(client, message):
     try:
         url = q[0]["link"]
     except BaseException:
-        return await rkp.edit("Failed search Song.")
+        return await rkp.edit("Şarkı Arama Başarısız.")
     type = "audio"
     if type == "audio":
         opts = {
@@ -58,41 +58,41 @@ async def song(client, message):
         }
         song = True
     try:
-        await rkp.edit("Downloading...")
+        await rkp.edit("indiriliyor...")
         with YoutubeDL(opts) as rip:
             rip_data = rip.extract_info(url)
     except DownloadError as DE:
         await rkp.edit(f"`{str(DE)}`")
         return
     except ContentTooShortError:
-        await rkp.edit("`The download content was too short.`")
+        await rkp.edit("`İndirme içeriği çok kısaydı.`")
         return
     except GeoRestrictedError:
         await rkp.edit(
-            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+            "`Bir web sitesi tarafından uygulanan coğrafi kısıtlamalar nedeniyle coğrafi konumunuzdan video alınamıyor.`"
         )
         return
     except MaxDownloadsReached:
-        await rkp.edit("`Max-downloads limit has been reached.`")
+        await rkp.edit("`Maksimum indirme sınırına ulaşıldı.`")
         return
     except PostProcessingError:
-        await rkp.edit("`There was an error during post processing.`")
+        await rkp.edit("`İşlem sonrası bir hata oluştu.`")
         return
     except UnavailableVideoError:
-        await rkp.edit("`Media is not available in the requested format.`")
+        await rkp.edit("`Medya istenen biçimde mevcut değil.`")
         return
     except XAttrMetadataError as XAME:
         await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
         return
     except ExtractorError:
-        await rkp.edit("`There was an error during info extraction.`")
+        await rkp.edit("`Bilgi çıkarma sırasında bir hata oluştu.`")
         return
     except Exception as e:
         await rkp.edit(f"{str(type(e)): {str(e)}}")
         return
     time.time()
     if song:
-        await rkp.edit("Uploading...") #levina-lab
+        await rkp.edit("yükleniyor...") #levina-lab
         lol = "./etc/thumb.jpg"
         lel = await message.reply_audio(
                  f"{rip_data['id']}.mp3",
